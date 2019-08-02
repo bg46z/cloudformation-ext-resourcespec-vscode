b8c57fa04939e8d36038cb4defdec4ecfe21762d
Zachary Bridges <zbridges@gmail.com>
2019-08-02 16:38:22 -0400

    Initial Commit

---
+++ src/extension.ts
@@ -0,0 +1,76 @@
+import * as vscode from 'vscode';
+import cloudformation from './CloudFormationResourceSpecification.original.json';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "cloudformation-ext-vscode" is now active!');
+
+	let resourceTypeProvider = vscode.languages.registerCompletionItemProvider( 'yaml', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			let keys : string[] = Object.keys(cloudformation.ResourceTypes);
+			var completions : vscode.CompletionItem[] = [];
+			
+			keys.forEach( key => {
+				completions.push(new vscode.CompletionItem(key.toString()));
+			});
+
+			return completions;
+		}
+	});
+
+	let propertyTypeProvider = vscode.languages.registerCompletionItemProvider( 'yaml', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			let keys : string[] = Object.keys(cloudformation.PropertyTypes);
+			var completions : vscode.CompletionItem[] = [];
+			
+			keys.forEach( key => {
+				completions.push(new vscode.CompletionItem(key.toString()));
+			});
+
+			return completions;
+		}
+	});
+
+	// let completionProvider2 = vscode.languages.registerCompletionItemProvider('plaintext', {
+
+	// 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+	// 		// a simple completion item which inserts `Hello World!`
+	// 		const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+	// 		// a completion item that inserts its text as snippet,
+	// 		// the `insertText`-property is a `SnippetString` which we will
+	// 		// honored by the editor.
+	// 		const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+	// 		snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+	// 		snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+
+	// 		// a completion item that can be accepted by a commit character,
+	// 		// the `commitCharacters`-property is set which means that the completion will
+	// 		// be inserted and then the character will be typed.
+	// 		const commitCharacterCompletion = new vscode.CompletionItem('console');
+	// 		commitCharacterCompletion.commitCharacters = ['.'];
+	// 		commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+	// 		// a completion item that retriggers IntelliSense when being accepted,
+	// 		// the `command`-property is set which the editor will execute after 
+	// 		// completion has been inserted. Also, the `insertText` is set so that 
+	// 		// a space is inserted after `new`
+	// 		const commandCompletion = new vscode.CompletionItem('new');
+	// 		commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+	// 		commandCompletion.insertText = 'new ';
+	// 		commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+	// 		// return all completion items as array
+	// 		return [
+	// 			simpleCompletion,
+	// 			snippetCompletion,
+	// 			commitCharacterCompletion,
+	// 			commandCompletion
+	// 		];
+	// 	}
+	// });
+
+	context.subscriptions.push(resourceTypeProvider, propertyTypeProvider);
+}
+
+export function deactivate() {}
---
+++ src/resourcespec.ts
@@ -0,0 +1,9 @@
+import data from './CloudFormationResourceSpecification.original.json';
+import * as fs from "fs";
+import * as vscode from 'vscode';
+import * as path from "path";
+const cloudformationresources = data.ResourceTypes;
+
+export function getResourceSpec(): any {
+}
+
