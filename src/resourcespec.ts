9d2484b47759ac7bd7520ec9cded0f756f2616f2
Zachary Bridges <zbridges@gmail.com>
2019-08-05 14:38:43 -0400

    add resource completions

--- src/resourcespec.ts
+++ src/resourcespec.ts
@@ -1,9 +1,46 @@
 import data from './CloudFormationResourceSpecification.original.json';
-import * as fs from "fs";
-import * as vscode from 'vscode';
-import * as path from "path";
-const cloudformationresources = data.ResourceTypes;
+const cf = data.ResourceTypes;
 
-export function getResourceSpec(): any {
+interface ICloudFormationResourceProperty {
+    PropertyName: string;
+    Documentation: string;
+}
+
+export class ResourceSpecCompletionItem {
+
+    private _completionItemName: string;
+    public get completionItemName(): string {
+        return this._completionItemName;
+    }
+    public set completionItemName(v: string) {
+        this._completionItemName = v;
+    }
+    
+    private _completionItemProperties : ICloudFormationResourceProperty[];
+    public get completionItemProperties() : ICloudFormationResourceProperty[] {
+        return this._completionItemProperties;
+    }
+
+    constructor(name: string) {
+        this._completionItemName = name;
+        this._completionItemProperties = this.getCompletionItemProperties(name);
+    }
+    
+    private getCompletionItemProperties(itemName: string) : ICloudFormationResourceProperty[] {
+        var properties = cf[itemName].Properties;
+        var requiredProperties : ICloudFormationResourceProperty[] = [];
+        var entries = Object.entries(properties);
+        entries.map( (entry : any ) => {
+            if ( entry[1].Required === true) {
+                requiredProperties.push(
+                    {
+                        PropertyName: entry[0],
+                        Documentation: entry[1].Documentation.toString()
+                    }
+                );
+            }
+        });
+        return requiredProperties;
+    }
 }
 
